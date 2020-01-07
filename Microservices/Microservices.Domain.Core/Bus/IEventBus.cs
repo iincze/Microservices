f6d4a5cbb9e627340fe4917a085a42f6eebac81d
Incze Istvan <iincze@yahoo.com>
2020-01-08 01:03:32 +0200

    end 15

---
+++ Microservices/Microservices.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+namespace Microservices.Domain.Core.Bus
+{
+    public interface IEventBus
+    {
+        Task SendCommand<T>(T command) where T : Command;
+
+        void Publish<T>(T @event) where T : Event;
+
+        void Subscribe<T, TH>()
+            where T : Event
+            where TH : IEventHandler<T>;
+    }
+}
---
+++ Microservices/Microservices.Domain.Core/Bus/IEventHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+namespace Microservices.Domain.Core.Bus
+{
+    public interface IEventHandler<in TEvent> : IEventHandler 
+        where TEvent : Event
+    {
+        Task Handle(TEvent @event);
+    }
+
+    public interface IEventHandler
+    {
+
+    }
+}
