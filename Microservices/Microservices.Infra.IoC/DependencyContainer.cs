615ebba194884b3e6ee2dd21e251c9f156edebf0
InczeIstvan <iinczexiii@gmail.com>
2020-01-10 15:03:04 +0200

    Initial Commit of Incrastructure Dependency Project

---
+++ Microservices/Microservices.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,15 @@
+﻿using Microservices.Domain.Core.Bus;
+using Microservices.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microservices.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
