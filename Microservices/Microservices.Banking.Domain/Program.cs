564ad2f3c5b6bf32274c798b99650c8fab8d2965
Incze Istvan <iincze@yahoo.com>
2020-01-12 13:50:12 +0200

    Initial Commit of Banking Microservice

---
+++ Microservices/Microservices.Banking.Domain/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Microservices.Banking.Domain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
